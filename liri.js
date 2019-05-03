fa8606e1c3cb1a11262c37148c01ca8d7fd0ef16
Kiha Moore <kihainusa@gmail.com>
2019-05-02 21:19:16 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+require("moment")
+require("axios")
+
+var keys = require("./keys.js");
+var nodeSpotifyApi = require("node-spotify-api");
+var moment = require("moment")
+
+var spotify = new Spotify(keys.spotify);
+
+var concertThis = function () {
+
+}
+
+var spotifyThisSong = function () {
+
+}
+
+var movieThis = function () {
+
+} 
+
+var doWhatItSays = function () {
+
+}
+
